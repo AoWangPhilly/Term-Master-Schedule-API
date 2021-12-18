ca6b79b76635475706f9a8f2bd08fa5af9a993d8
AoWangPhilly <aw3338@drexel.edu>
2021-12-18 17:59:51 -0500

    Grab quarter term links and school links with jsessionid's

--- scraper/scaper.py
+++ scraper/scaper.py
@@ -1,11 +1,31 @@
+from pprint import pprint
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from requests.cookies import RequestsCookieJar
 
+from cleaner import clean_subjects
+
 BASE_URL = "https://termmasterschedule.drexel.edu/"
 SCHOOL_URL = f"{BASE_URL}webtms_du/collegesSubjects/202115?collCode=CI"
 MAX_THREADS = 30
+
+
+def get_quarter_term_links(url: str) -> list[str]:
+    """Get links for quarter term courses on the front page"""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    term_panel_anchors = soup.find("table", class_="termPanel").findAll("a")
+    return [f"{BASE_URL}{a['href']}" for a in term_panel_anchors]
+
+
+def get_school_links(url: str) -> list[str]:
+    """Get the available school links on the side for each quarter term"""
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    school_panel = soup.find("div", id="sideLeft").findAll("a")
+    return [f"{BASE_URL}{a['href']}" for a in school_panel]
 
 
 def get_subjects_links(url: str) -> tuple[list[str], RequestsCookieJar]:
@@ -17,29 +37,21 @@ def get_subjects_links(url: str) -> tuple[list[str], RequestsCookieJar]:
     return [f"{BASE_URL}{a['href']}" for a in subject_anchors], cookies
 
 
-def get_courses(url: str, cookies: RequestsCookieJar) -> pd.DataFrame:
-    header = ["Subject Code", "Course Number", "Instr. Type", "Instr. Method", "Section", "CRN", "Course Title",
-              "Days/Time", "Instructor"]
+def get_courses(url: str, cookies: str) -> pd.DataFrame:
     df = pd.read_html(url, attrs={"id": "sortableTable"})[0]
-    print(url)
-    print(df.columns)
+    df = clean_subjects(df)
     # df.to_csv(path_or_buf=url[url.find("courseList") + 11: url.find(";")] + ".csv", index=False)
     return df
 
 
-def get_quarter_term_links(url: str) -> list[str]:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    term_panel_anchors = soup.find("table", class_="termPanel").findAll("a")
-    return [f"{BASE_URL}{a['href']}" for a in term_panel_anchors]
-
+# ;jsessionid=3CCF59D7618B76817516A1B6A1B56141
 
 def main():
-    print(get_quarter_term_links(BASE_URL))
+    quarters = get_quarter_term_links(BASE_URL)
+    pprint(get_school_links(quarters[1]))
     # subject_urls, cookie = get_subjects_links(SCHOOL_URL)
-    # subject_links = [f"{BASE_URL}{link}" for link in subject_urls]
-    # pd.set_option('max_columns', None)
-    # print(get_courses(subject_links[0], cookies=cookie))
+    # print(get_courses(subject_urls[1], cookies=cookie))
+    # print(cookie.values())
     # t0 = time.time()
     # # for link in subject_urls:
     # #     get_courses(f"{BASE_URL}{link}", cookie)
