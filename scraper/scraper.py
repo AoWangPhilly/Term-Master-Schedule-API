bb657f61435a206e9499541c415f535d142c7db9
AoWangPhilly <aw3338@drexel.edu>
2021-12-19 14:25:53 -0500

    Rename from scaper to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -38,7 +38,7 @@ def get_subjects_links(url: str) -> list[str]:
     return [f"{BASE_URL}{a['href']}" for a in subject_anchors]
 
 
-def get_courses(url: str) -> None:
+def get_courses(coll_code: str, url: str) -> None:
     df = pd.read_html(url, attrs={"id": "sortableTable"})[0]
     df = clean_subjects(df)
 
@@ -47,15 +47,19 @@ def get_courses(url: str) -> None:
     term_name = soup.find(text=re.compile(r"^Schedule for.*"))
     term_name = term_name[term_name.find("(") + 1: term_name.find(")")]
     df["term"] = int(term_name)
-
+    # df["id"] = df["crn"] + df["term"]
+    df["coll_code"] = coll_code
     df.to_sql("quarters", engine, if_exists="append", index=False)
+    # return df
 
 
 def save_subjects(school_url: str) -> None:
-    subject_urls = get_subjects_links(school_url)
-    threads = min(30, len(subject_urls))
+    coll_code = school_url[school_url.find("collCode") + 9:]
+    urls = get_subjects_links(school_url)
+    threads = min(30, len(urls))
+    args = ((coll_code, url) for url in urls)
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.map(get_courses, subject_urls)
+        executor.map(lambda p: get_courses(*p), args)
 
 
 def save_subjects_per_school(term_url: str) -> None:
@@ -75,6 +79,14 @@ def main():
     save_subjects_per_term()
     t1 = time.time()
     print(f"{t1 - t0} seconds to scrape.")
+
+    # quarters = get_quarter_term_links(BASE_URL)
+    # fall = quarters[0]
+    #
+    # schools_in_fall = get_school_links(fall)
+    # ci = schools_in_fall[5]
+    # subjects_in_ci = get_subjects_links(ci)
+    # print(get_courses(subjects_in_ci[0]))
 
 
 if __name__ == "__main__":
