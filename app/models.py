9615f05b27d3d091be81d68d7f2518b8fb1f721e
AoWangPhilly <aw3338@drexel.edu>
2021-12-19 14:25:43 -0500

    Update ORM model

--- app/models.py
+++ app/models.py
@@ -6,14 +6,14 @@ from .database import Base
 
 class Quarter(Base):
     __tablename__ = "quarters"
-
+    crn = Column(Integer, nullable=False, primary_key=True)
+    term = Column(Integer, nullable=False, primary_key=True)
     subject_code = Column(String, nullable=False)
-    course_number = Column(Integer, nullable=False)
+    course_number = Column(String, nullable=False)
     instr_type = Column(String, nullable=False)
     instr_method = Column(String, nullable=False)
     section = Column(String, nullable=False)
-    crn = Column(Integer, nullable=False, primary_key=True)
     course_title = Column(String, nullable=False)
     meet_time = Column(ARRAY(String))
     instructor = Column(String, nullable=False)
-    term = Column(Integer, nullable=False)
+    coll_code = Column(String, nullable=False)
