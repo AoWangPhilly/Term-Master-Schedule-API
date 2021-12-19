7dd0156982b37abeb21d86a86386befdd4e995ae
AoWangPhilly <aw3338@drexel.edu>
2021-12-18 23:00:41 -0500

    Add router to main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
+from .routers import quarter
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.include_router(quarter.router)
 
 
 @app.get("/")
