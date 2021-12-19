2cc670bef8650ec023fcf4f05e54d8b22cc13689
AoWangPhilly <aw3338@drexel.edu>
2021-12-19 14:25:11 -0500

    Get credit, course description, and prereq

---
+++ scraper/catalog.py
@@ -0,0 +1,39 @@
+import re
+from typing import Optional, List
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_url(subject_code: str, course_number: str) -> str:
+    return f"http://catalog.drexel.edu/search/?P={subject_code}%20{course_number}"
+
+
+class CourseCatalog:
+    def __init__(self, subject_code: str, course_number: str):
+        self.subject_code = subject_code
+        self.course_number = course_number
+        self.url = get_url(subject_code, course_number)
+        self.page = requests.get(self.url)
+        self.content = BeautifulSoup(self.page.content, "html.parser")
+
+    def find_credit(self) -> List[float]:
+        results = self.content.find("div", class_="searchresult search-courseresult")
+        header = results.next_element.text
+        return list(map(float, re.findall(r"\d+\.\d+", header)))
+
+    def find_course_description(self) -> str:
+        return self.content.find("p", class_="courseblockdesc").text
+
+    def find_preqs(self) -> Optional[str]:
+        prereq_block = self.content.find("b", text="Prerequisites:")
+        if not prereq_block:
+            return None
+        return prereq_block.next_sibling.strip()
+
+
+if __name__ == "__main__":
+    course_catalog = CourseCatalog("HNRS", "T480")
+    print(course_catalog.find_course_description())
+    print(course_catalog.find_preqs())
+    print(course_catalog.find_credit())
