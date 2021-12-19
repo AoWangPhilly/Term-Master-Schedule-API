fe8684dc2885ee7c6c13cf2f38c0bd1cf0c9eb53
AoWangPhilly <aw3338@drexel.edu>
2021-12-18 23:00:30 -0500

    Create router for API

---
+++ app/routers/quarter.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, status, HTTPException
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+
+from app import models
+from app.database import get_db
+
+router = APIRouter(
+    prefix="/quarters",
+    tags=["Quarters"]
+)
+
+
+@router.get("/{term}")
+def get_quarter(term: int, db: Session = Depends(get_db)):
+    quarter = db.query(models.Quarter).filter(models.Quarter.term == term).all()
+    if not quarter:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There's no term with {term}")
+    return quarter
