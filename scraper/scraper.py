940b0b7c80606843588751250b89070fddae7478
AoWangPhilly <aw3338@drexel.edu>
2021-12-19 20:57:08 -0500

    Grab term faster

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,4 @@
 import concurrent.futures
-import re
 import time
 
 import pandas as pd
@@ -38,26 +37,28 @@ def get_subjects_links(url: str) -> list[str]:
     return [f"{BASE_URL}{a['href']}" for a in subject_anchors]
 
 
-def get_courses(coll_code: str, url: str) -> None:
+def get_courses(term: int, coll_code: str, url: str) -> None:
     df = pd.read_html(url, attrs={"id": "sortableTable"})[0]
     df = clean_subjects(df)
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    term_name = soup.find(text=re.compile(r"^Schedule for.*"))
-    term_name = term_name[term_name.find("(") + 1: term_name.find(")")]
-    df["term"] = int(term_name)
-    # df["id"] = df["crn"] + df["term"]
+    df["term"] = term
     df["coll_code"] = coll_code
+    # df["credit"] = df.apply(lambda x: CourseCatalog(x.subject_code, x.course_number).find_credit(), axis=1)
+    # df["course_desc"] = df.apply(lambda x: CourseCatalog(x.subject_code, x.course_number).find_course_description(),
+    #                              axis=1)
+    # df["prereq"] = df.apply(lambda x: CourseCatalog(x.subject_code, x.course_number).find_preqs(), axis=1)
+    # print(df.columns)
     df.to_sql("quarters", engine, if_exists="append", index=False)
     # return df
 
 
 def save_subjects(school_url: str) -> None:
-    coll_code = school_url[school_url.find("collCode") + 9:]
+    coll_code = school_url[school_url.find("collCode") + len("collCode") + 1:]
+    term = int(school_url[school_url.find("collegesSubjects") + len("collegesSubjects") + 1: school_url.find(";")])
     urls = get_subjects_links(school_url)
     threads = min(30, len(urls))
-    args = ((coll_code, url) for url in urls)
+    args = ((term, coll_code, url) for url in urls)
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         executor.map(lambda p: get_courses(*p), args)
 
@@ -86,7 +87,7 @@ def main():
     # schools_in_fall = get_school_links(fall)
     # ci = schools_in_fall[5]
     # subjects_in_ci = get_subjects_links(ci)
-    # print(get_courses(subjects_in_ci[0]))
+    # print(get_courses(202115, "CI", subjects_in_ci[0]))
 
 
 if __name__ == "__main__":
