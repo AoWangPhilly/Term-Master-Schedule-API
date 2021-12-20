dd90290eef94c0ca6ba17f139865c094a2772a6a
AoWangPhilly <aw3338@drexel.edu>
2021-12-20 13:50:20 -0500

    Getting class details

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,15 @@
+"""
+Term Master Schedule Scraper
+----------------------------
+The module scrapes Drexel's TMS from Fall Quarter 20-21 to Summer Quarter 21-22
+in a little over a minute. It'll grab all subjects from each available school,
+taking the subject_code, course_number, instruction type and method, section,
+crn, course title, days / time, and instructor saving it all to a PostgreSQL db.
+"""
+
 import concurrent.futures
 import time
+from typing import List, Any
 
 import pandas as pd
 import requests
@@ -9,8 +19,6 @@ from app.database import engine
 from cleaner import clean_subjects
 
 BASE_URL = "https://termmasterschedule.drexel.edu/"
-SCHOOL_URL = f"{BASE_URL}webtms_du/collegesSubjects/202115?collCode=CI"
-MAX_THREADS = 30
 
 
 def get_quarter_term_links(url: str) -> list[str]:
@@ -30,57 +38,97 @@ def get_school_links(url: str) -> list[str]:
 
 
 def get_subjects_links(url: str) -> list[str]:
+    """Get subject's link in specific school"""
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content, "html.parser")
     subject_anchors = soup.find("table", class_="collegePanel").findAll("a")
     return [f"{BASE_URL}{a['href']}" for a in subject_anchors]
 
 
 def get_courses(term: int, coll_code: str, url: str) -> None:
+    """Reads in the subject's table and cleans it up"""
     df = pd.read_html(url, attrs={"id": "sortableTable"})[0]
     df = clean_subjects(df)
+
+    # # Previous idea of also grabbing course details
+    # details = get_all_course_details(url)
+    # df = df.merge(details, on="crn")
     df["term"] = term
     df["coll_code"] = coll_code
-    df.to_sql("quarters1", engine, if_exists="append", index=False)
+    df.to_sql("quarters", engine, if_exists="append", index=False)
 
 
-# ;jsessionid=69F46BFACD2486F0F33E1D5BF381D785
+def get_course_details_urls(url) -> List[str]:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    a_tags = soup.find("table", id="sortableTable").findAll("a")
+    hrefs = [f"{BASE_URL}{a['href']}" for a in a_tags]
+    return hrefs
+
+
+def get_course_details(url) -> dict[str, Any]:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    crn = int(soup.find("td", text=r"CRN").findNext("td").text)
+    credits = float(soup.find("td", text="Credits").findNext("td").text.strip())
+    max_enroll = int(soup.find("td", text="Max Enroll").findNext("td").text)
+    details = {"crn": crn,
+               "credits": credits,
+               "max_enroll": max_enroll}
+
+    enroll = soup.find("td", text="Enroll").findNext("td").text.strip()
+    if enroll == "CLOSED":
+        enroll = details["max_enroll"]
+    else:
+        enroll = int(enroll)
+
+    course_desc = soup.find("div", class_="courseDesc").text.strip()
+
+    prereqs = soup.find("b", text="Pre-Requisites:").findParent().findAll("span")
+    prereqs = ' '.join([prereq.text.strip() for prereq in prereqs])
+
+    details = {"crn": crn,
+               "credits": credits,
+               "max_enroll": max_enroll,
+               "enroll": enroll,
+               "course_desc": course_desc,
+               "prereqs": prereqs}
+    return details
+
+
+def get_all_course_details(url):
+    urls = get_course_details_urls(url)
+    output_df = pd.DataFrame(columns=["crn", "credits", "max_enroll", "enroll", "course_desc", "prereqs"])
+
+    for url in urls:
+        output_df = output_df.append(get_course_details(url), ignore_index=True)
+    return output_df
+
+
 def save_subjects(school_url: str) -> None:
     coll_code = school_url[school_url.find("collCode") + len("collCode") + 1:]
     term = int(school_url[school_url.find("collegesSubjects") + len("collegesSubjects") + 1: school_url.find(";")])
     urls = get_subjects_links(school_url)
-    threads = min(MAX_THREADS, len(urls))
     args = ((term, coll_code, url) for url in urls)
 
-    # for arg in args:
-    #     print(arg)
-    #     get_courses(arg[0], arg[1], arg[2])
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(lambda p: get_courses(*p), args)
 
 
-def save_subjects_per_school(term_url: str) -> None:
-    schools = get_school_links(term_url)
-    for school in schools:
-        save_subjects(school_url=school)
-
-
-def save_subjects_per_term() -> None:
-    quarters = get_quarter_term_links(BASE_URL)
+def save_subjects_per_term(url) -> None:
+    quarters = get_quarter_term_links(url=url)
     for quarter in quarters:
-        save_subjects_per_school(term_url=quarter)
+        schools = get_school_links(url=quarter)
+        for school in schools:
+            save_subjects(school_url=school)
 
 
 def main():
     t0 = time.time()
-    save_subjects_per_term()
+    save_subjects_per_term(BASE_URL)
     t1 = time.time()
     print(f"{t1 - t0} seconds to scrape.")
-    # 503.6655421257019 seconds to scrape.
-    # 26,903
-
-    # 26,406 26,409
 
 
 if __name__ == "__main__":
