36b41c641e6a77dcb8112b07acdaebd36aa23307
AoWangPhilly <aw3338@drexel.edu>
2021-12-20 13:50:42 -0500

    Catch errors

--- scraper/catalog.py
+++ scraper/catalog.py
@@ -1,4 +1,3 @@
-import concurrent.futures
 import re
 from typing import Optional, List, Tuple
 
@@ -23,22 +22,34 @@ class CourseCatalog:
         self.content = BeautifulSoup(self.page.content, "html.parser")
 
     def find_credit(self) -> List[float]:
-        results = self.content.find("div", class_="searchresult search-courseresult")
-        header = results.next_element.text
-        credit = list(map(float, re.findall(r"\d+\.\d+", header)))
+        try:
+            results = self.content.find("div", class_="searchresult search-courseresult")
+            header = results.next_element.text
+            credit = list(map(float, re.findall(r"\d+\.\d+", header)))
 
-        return credit
+            return credit
+        except Exception as error:
+            print(f"Cannot find number of credits for {self.course}.", error)
+            return None
 
     def find_course_description(self) -> str:
-        course_desc = self.content.find("p", class_="courseblockdesc").text.strip()
-        return course_desc
+        try:
+            course_desc = self.content.find("p", class_="courseblockdesc").text.strip()
+            return course_desc
+        except Exception as error:
+            print(f"Cannot find course description for {self.course}.", error)
+            return None
 
     def find_preqs(self) -> Optional[str]:
-        prereq_block = self.content.find("b", text="Prerequisites:")
-        if not prereq_block:
+        try:
+            prereq_block = self.content.find("b", text="Prerequisites:")
+            if not prereq_block:
+                return None
+            preq = prereq_block.next_sibling.strip()
+            return preq
+        except Exception as error:
+            print(f"Cannot find prereqs for {self.course}.", error)
             return None
-        preq = prereq_block.next_sibling.strip()
-        return preq
 
 
 def get_course_info(subject_code: str, course_number: str):
@@ -62,15 +73,3 @@ def insert_course_data_to_db(subject_code: str, course_number: str):
     query = db.insert(courses).values(subject_code=subject_code, course_number=course_number, credit=credit,
                                       course_desc=course_desc, prereq=prereq)
     connection.execute(query)
-
-
-def get_all_course_info() -> None:
-    courses = get_unique_courses()
-    threads = min(30, len(courses))
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.map(lambda p: insert_course_data_to_db(*p), courses)
-
-
-if __name__ == "__main__":
-    get_all_course_info()
